248935997a7231a6d4b3fd6e41ca668425c65ff9
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-12-21 11:49:29 +0700

    Create dashboard page and quote components

---
+++ src/app/dashboard.js
@@ -0,0 +1,14 @@
+
+
+angular
+  .module('app')
+  .component('dashboard', {
+    templateUrl: 'app/dashboard.html',
+    controller: dashboardController,
+    controllerAs: 'vm'
+  });
+
+function dashboardController() {
+  vm = this;
+  
+}
