5659b56f18a6ad7064ea6bd0a53a10cfda59ecd5
Zuhra Sofyan <zuhrasofyan@gmail.com>
2016-12-23 00:04:57 +0700

    Implement logout button

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -1,5 +1,24 @@
 angular
   .module('app')
   .component('fountainHeader', {
-    templateUrl: 'app/components/header/header.html'
+    templateUrl: 'app/components/header/header.html',
+    controller: headerController,
+    controllerAs: 'vm'
   });
+
+function headerController($rootScope, store, $location, authManager) {
+  var vm = this;
+
+  // if(store.get('user')){
+  //   vm.username = store.get('user').username;
+  // } else vm.username = null;
+
+
+  function logout(){
+    store.remove('user');
+    store.remove('token');
+    $location.path('/');
+    //authManager.
+  }
+  vm.logout = logout;
+}
